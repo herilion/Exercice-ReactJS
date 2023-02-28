25104e48a28345888dba6243d60c343b5dc9c66f
herilion <heritierlionge@yahoo.com>
2023-02-28 13:45:22 +0200

    refactor cart4

--- src/composants/Cart.jsx
+++ src/composants/Cart.jsx
@@ -37,9 +37,7 @@ function Cart() {
                 <p>Proverbe</p>
                 <div className='group1'>
                     <p>70%</p>
-                    <div className='bigDiv'>
-                        <div className='smBig'></div>
-                    </div>
+                    <progress max="100" value="70"></progress>
                 </div>
             </div>
         </div>
