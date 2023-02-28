dcb85d51f2c3909ec722d7ef39670a0ae1e747d5
herilion <heritierlionge@yahoo.com>
2023-02-28 13:44:56 +0200

    refactor cart3

--- src/composants/Cart.jsx
+++ src/composants/Cart.jsx
@@ -30,9 +30,7 @@ function Cart() {
                 <p>Philemon</p>
                 <div className='group1'>
                     <p>70%</p>
-                    <div className='bigDiv'>
-                        <div className='smBig'></div>
-                    </div>
+                    <progress max="100" value="70"></progress>
                 </div>
             </div>
             <div className='cart1'>
