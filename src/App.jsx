79be28dcad65c938e6c0f720bf259927b5c83544
herilion <heritierlionge@yahoo.com>
2023-02-28 11:54:10 +0200

    feat: add carts

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Cart from './composants/Cart'
+function App() {
+  return (
+    <div className="App">
+      <Cart />
+    </div>
+  )
+}
+
+export default App
---
+++ src/composants/Cart.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Cart.css'
+
+function Cart() {
+    return (
+        <div>
+            <div className='cart1'>
+                <p>Proverbe</p>
+                <div className='group1'>
+                    <p>70%</p>
+                    <div className='bigDiv'>
+                        <div className='smBig'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='cart1'>
+                <p>tite</p>
+                <div className='group1'>
+                    <p>70%</p>
+                    <div className='bigDiv'>
+                        <div className='smBig'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='cart1'>
+                <p>Philemon</p>
+                <div className='group1'>
+                    <p>70%</p>
+                    <div className='bigDiv'>
+                        <div className='smBig'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='cart1'>
+                <p>Proverbe</p>
+                <div className='group1'>
+                    <p>70%</p>
+                    <div className='bigDiv'>
+                        <div className='smBig'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Cart
